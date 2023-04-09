e58273a5be5d9a80572b05db5d7acaac41d119f2
Alec Zinsli <zinsli.alec@gmail.com>
2023-04-08 21:53:22 -0400

    function

--- pipeline.py
+++ pipeline.py
@@ -12,23 +12,29 @@ import logging
 import subprocess
 logging.basicConfig(level=logging.ERROR)
 
+def apply_taa(depth_frame, taa_strength=0.5):
+    if apply_taa.previous_frame is None:
+        apply_taa.previous_frame = depth_frame.copy()
+    taa_frame = (taa_strength * apply_taa.previous_frame + (1 - taa_strength) * depth_frame).astype(np.uint8)
+    apply_taa.previous_frame = depth_frame.copy()
+    return taa_frame
+
+apply_taa.previous_frame = None
+
 def pipeline(prediction, color_frame, taa_strength=0.5):
     # Use depth_to_rgb24_pixels to convert prediction to a depth_frame
     depth_frame = depth_to_rgb24(prediction)
 
-    # Resize to match color frame
-    depth_frame = cv2.resize(depth_frame, (color_frame.shape[1], color_frame.shape[0]), interpolation=cv2.INTER_NEAREST)
-
     # Apply TAA to depth frame
-    if 'previous_frame' not in pipeline.__dict__:
-        pipeline.previous_frame = depth_frame
-    depth_frame = (taa_strength * pipeline.previous_frame + (1 - taa_strength) * depth_frame).astype(np.uint8)
-    pipeline.previous_frame = depth_frame.copy()
+    taa_frame = apply_taa(depth_frame, taa_strength=taa_strength)
+
+    # Resize to match color frame
+    resized_frame = cv2.resize(taa_frame, (color_frame.shape[1], color_frame.shape[0]), interpolation=cv2.INTER_NEAREST)
 
     # Convert color frame from RGB to BGR format for cv2
     color_frame = (color_frame * 255).astype(np.uint8)
     color_frame = cv2.cvtColor(color_frame, cv2.COLOR_RGB2BGR)
 
     # Combine color and depth frames
-    combined_frame = np.concatenate((color_frame, depth_frame), axis=1)
+    combined_frame = np.concatenate((color_frame, resized_frame), axis=1)
     return combined_frame
