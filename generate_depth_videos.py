e862f0a55fe9082db3db6189635c8ec164a950f6
Alec Zinsli <zinsli.alec@gmail.com>
2023-04-09 13:16:19 -0400

    added more depth filtering

--- generate_depth_videos.py
+++ generate_depth_videos.py
@@ -14,17 +14,6 @@ logging.basicConfig(level=logging.ERROR)
 
 saved_output = False
 
-def clear_output_folder(folder_path):
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print(f"Failed to delete {file_path}. Reason: {e}")
-
 def get_metadata(video_path):
     """Initialize video reader and get metadata."""
     video_capture = cv2.VideoCapture(video_path)
@@ -47,7 +36,7 @@ def initialize_output(output_folder, frame_size, fps, video_path):
     video_filename = os.path.basename(video_path)
     output_video_filename = os.path.join(output_folder, video_filename.replace(os.path.splitext(video_filename)[1], '_output.mp4'))
     output_args = ['-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-crf', '18', '-maxrate', '8M', '-bufsize', '512M']
-    ffmpeg_cmd = ['ffmpeg', '-y', '-f', 'rawvideo', '-pix_fmt', 'bgr24', '-s', f"{frame_size[0]}x{frame_size[1]}", '-r', str(fps), '-i', '-', '-i', video_path, '-c:v', 'copy', '-c:a', 'aac', '-map', '0:v:0', '-map', '1:a:0', '-muxdelay', '0', *output_args, output_video_filename]
+    ffmpeg_cmd = ['ffmpeg', '-y', '-f', 'rawvideo', '-pix_fmt', 'rgb24', '-s', f"{frame_size[0]}x{frame_size[1]}", '-r', str(fps), '-i', '-', '-i', video_path, '-c:v', 'copy', '-c:a', 'aac', '-map', '0:v:0', '-map', '1:a:0', '-muxdelay', '0', *output_args, output_video_filename]
     video_writer = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print(f"Output video size: {frame_size[1]} x {frame_size[0]}")
     print("ffmpeg command: ", ' '.join(ffmpeg_cmd))
@@ -60,15 +49,37 @@ def write_output_video(predictions, frames, video_writer):
         if combined_frame is not None:
             video_writer.stdin.write(combined_frame.tobytes())
 
+def resize_frame(frame, min_width=384):
+    height, width, _ = frame.shape
+    if width >= min_width:
+        return frame.copy()
+
+    new_width = min_width
+    aspect_ratio = float(height) / float(width)
+    new_height = int(new_width * aspect_ratio)
+
+    return cv2.resize(frame.copy(), (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+
+def resize_to_original_size(predictions, original_frame_size):
+    resized_predictions = []
+
+    for pred in predictions:
+        resized_pred = cv2.resize(pred, original_frame_size[::-1], interpolation=cv2.INTER_AREA)
+        resized_predictions.append(resized_pred)
+
+    return resized_predictions
+
 def process_video(video_path, output_folder, model_path, model_type, optimize=False, height=None, square=False, batch_size=8):
     # Initialize the model
     device, model, transform, net_w, net_h = initialize_model(model_path, model_type, optimize, height, square)
 
     # Initialize video reader and writer
     video_reader, fps, frame_size, frame_count = initialize_video(video_path)
+    frame_size_original = frame_size
 
     # Double the width of the frame size to accommodate the pipeline function
-    frame_size = (frame_size[0]*2, frame_size[1])
+    frame_size = (frame_size[0] * 2, frame_size[1])
 
     video_writer, output_video_filename = initialize_output(output_folder, frame_size, fps, video_path)
 
@@ -82,14 +93,20 @@ def process_video(video_path, output_folder, model_path, model_type, optimize=Fa
 
     # Iterate over video frames and process them in batches
     for i, frames in enumerate(batch_process(video_reader, batch_size)):
-        # Process the frames and get depth predictions
+        # Resize the frames while maintaining the aspect ratio
+        resized_frames = [resize_frame(frame) for frame in frames]
+
+        # Process the resized frames and get depth predictions
         start_time = time.time()
-        frames = np.array(frames, dtype=np.float64) / 255.0
+        resized_frames = np.array(resized_frames, dtype=np.float64) / 255.0
         time_frame_processing += time.time() - start_time
 
         start_time = time.time()
-        predictions = process_images(device, model, model_type, transform, net_w, net_h, frames, optimize)
+        predictions = process_images(device, model, model_type, transform, net_w, net_h, resized_frames, optimize)
         time_depth_prediction += time.time() - start_time
+
+        # Resize predictions and frames back to the original size
+        predictions = resize_to_original_size(predictions, frame_size)
 
         # Write combined frames to output video
         start_time = time.time()
@@ -109,28 +126,26 @@ def process_video(video_path, output_folder, model_path, model_type, optimize=Fa
     print(f"Time taken for depth prediction: {time_depth_prediction:.2f} seconds with a batch_size of {batch_size}")
     print(f"Time taken for writing output video: {time_output_video:.2f} seconds")
 
+
 def is_video_file(filename):
     supported_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.wmv', '.flv', '.wedbm', '.m4v', '.3gp']
     _, file_extension = os.path.splitext(filename)
     return file_extension.lower() in supported_extensions
 
 if __name__ == "__main__":
-    output_folder = "out/depth_videos/"
+    output_folder = "output/"
 
     model_type = "dpt_beit_base_384"
-    # model_type = "dpt_beit_large_384"
+    # model_type = "dpt_swin2_base_384"
     # model_type = "dpt_beit_large_512"
 
     model_path = f"weights/{model_type}.pt"
     optimize = True
     height = None
     square = False
-    batch_size = 32
+    batch_size = 16
 
     input_folder = "unformatted_videos/"
-
-    # Clear the output folder before rendering
-    clear_output_folder(output_folder)
 
     for filename in os.listdir(input_folder):
         if is_video_file(filename):
--- pipeline.py
+++ pipeline.py
@@ -12,6 +12,27 @@ import logging
 import subprocess
 logging.basicConfig(level=logging.ERROR)
 
+def apply_temporal_median_filter(depth_frame, buffer_size=5):
+    if apply_temporal_median_filter.buffer is None:
+        apply_temporal_median_filter.buffer = [depth_frame.copy()] * buffer_size
+
+    apply_temporal_median_filter.buffer.pop(0)
+    apply_temporal_median_filter.buffer.append(depth_frame.copy())
+    
+    median_frame = np.median(apply_temporal_median_filter.buffer, axis=0).astype(np.uint8)
+    return median_frame
+
+apply_temporal_median_filter.buffer = None
+
+def apply_bilateral_filter(depth_frame, diameter=5, sigma_color=75, sigma_space=75):
+    return cv2.bilateralFilter(depth_frame, diameter, sigma_color, sigma_space)
+
+def bias_depth_frame(depth_frame, bias_center=0.5, scale_factor=2.0):
+    depth_frame = depth_frame.astype(np.float32) / 255.0
+    depth_frame = (np.tanh((depth_frame - bias_center) * scale_factor) + 1.0) / 2.0
+    depth_frame = (depth_frame * 255).astype(np.uint8)
+    return depth_frame
+
 def apply_taa(depth_frame, taa_strength=0.5):
     if apply_taa.previous_frame is None:
         apply_taa.previous_frame = depth_frame.copy()
@@ -19,21 +40,37 @@ def apply_taa(depth_frame, taa_strength=0.5):
     apply_taa.previous_frame = depth_frame.copy()
     return taa_frame
 
+def apply_dilation(depth_frame, kernel_size=3, iterations=1):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    return cv2.dilate(depth_frame, kernel, iterations=iterations)
+
 apply_taa.previous_frame = None
 
-def pipeline(prediction, color_frame, taa_strength=0.5):
+def remove_background_gradient(depth_frame, threshold=128):
+    _, depth_frame = cv2.threshold(depth_frame, threshold, 255, cv2.THRESH_BINARY)
+    return depth_frame
+
+def pipeline(prediction, color_frame, taa_strength=0.5, background_threshold=128):
     # Use depth_to_rgb24_pixels to convert prediction to a depth_frame
     depth_frame = depth_to_rgb24(prediction)
 
-    # Apply TAA to depth frame
-    taa_frame = apply_taa(depth_frame, taa_strength=taa_strength)
+    # Bias depth_frame towards 0.5
+    depth_frame = bias_depth_frame(depth_frame)
+
+    # Apply Bilateral Filter
+    depth_frame = apply_bilateral_filter(depth_frame)
+
+    # Apply Dilation to expand bright edges
+    depth_frame = apply_dilation(depth_frame, kernel_size=3, iterations=1)
+
+    # Remove background gradient
+    # depth_frame = remove_background_gradient(depth_frame, threshold=background_threshold)
+
+    # Apply TAA
+    depth_frame = apply_taa(depth_frame, taa_strength=taa_strength)
 
     # Resize to match color frame
-    resized_frame = cv2.resize(taa_frame, (color_frame.shape[1], color_frame.shape[0]), interpolation=cv2.INTER_NEAREST)
-
-    # Convert color frame from RGB to BGR format for cv2
-    color_frame = (color_frame * 255).astype(np.uint8)
-    color_frame = cv2.cvtColor(color_frame, cv2.COLOR_RGB2BGR)
+    resized_frame = cv2.resize(depth_frame, (color_frame.shape[1], color_frame.shape[0]), interpolation=cv2.INTER_NEAREST)
 
     # Combine color and depth frames
     combined_frame = np.concatenate((color_frame, resized_frame), axis=1)
